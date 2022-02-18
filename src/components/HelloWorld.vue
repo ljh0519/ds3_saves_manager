9b0ea8fce2fb638dbbf3e2edbe49780a4c572ff1
zhangting <273664273@qq.com>
2022-02-18 23:49:00 +0800

    v1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,203 @@
 <template>
-  <div >
+  <div>
     <div class="top">
       <div class="left">
-       <van-button plain type="primary" size="large">备份</van-button>
+        <el-button type="primary" plain class="save">主要按钮</el-button>
       </div>
       <div class="right">
-       <van-button plain type="primary" size="large">还原</van-button>
+        <el-button type="primary" plain class="restore">主要按钮</el-button>
       </div>
-   
     </div>
 
     <div class="table">
-
+      <el-table :data="tableData" style="{width: 100%}"   height="100%">
+        <el-table-column fixed prop="date" label="日期" >
+        </el-table-column>
+        <el-table-column prop="save" label="存档" >
+        </el-table-column>
+        <el-table-column prop="steam_id" label="steam-id" >
+        </el-table-column>
+        <el-table-column prop="location" label="位置" >  </el-table-column>
+        <el-table-column fixed="right" label="操作" >
+          <template slot-scope="scope">
+            <el-button
+              @click.native.prevent="deleteRow(scope.$index, tableData)"
+              type="text"
+              size="small"
+            >
+              移除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
- 
-}
+  name: "HelloWorld",
+  methods: {
+    deleteRow(index, rows) {
+      rows.splice(index, 1);
+    },
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }, {
+          date: "2016-05-03",
+          save: "111",
+          steam_id: "bhfjdasu3",
+          location: "D/abc"
+        }
+       
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-  .top {
-    width: 100vw;
-    height: 30vh;
-    border:1px solid black;
+.top {
+  width: 100vw;
+  height: 25vh;
+  // margin-bottom: 4vh;
+  .left {
+    display: inline-block;
+    .save {
+      width: 50vw;
+      height: 30vh;
 
-   
+      font-size: 50px;
+    }
   }
+  .right {
+    display: inline-block;
+    .restore {
+      width: 50vw;
+      height: 30vh;
+      font-size: 50px;
+    }
+  }
+}
+.table {
+  height: 65vh;
+  .el-table_2_column_6 {
+    width: 15%;
+  }
+  .el-table_2_column_7 {
+    width: 20%;
+  }
+  .el-table_2_column_8 {
+    width: 15%;
+  }
+  .el-table_2_column_9 {
+    width: 40%;
+  }
+  .el-table_2_column_10 {
+    width: 10%;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import { Button } from 'vant';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
-Vue.use(Button);
+Vue.use(ElementUI);
 new Vue({
+  el: '#app',
   render: h => h(App),
 }).$mount('#app')
